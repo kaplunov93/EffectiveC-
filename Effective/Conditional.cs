e8cc363acd6d04e2bfdaca10795cce8c6b52ab9e
Aleksander Kaplunov <kaplunov93@gmail.com>
2015-08-10 16:02:01 +0300

    Chapter 1 Item 4

---
+++ Effective/Conditional.cs
@@ -0,0 +1,47 @@
+﻿#define DEBUG
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace Effective
+{
+    namespace conditional
+    {
+        class Class
+        {
+            private string someText;
+
+            public string SomeText
+            {
+                get
+                {
+                    CheckState();
+                    return someText;
+                }
+                set
+                {
+                    CheckState();
+                    someText = value;
+                    CheckState();
+                }
+            }
+
+            void CheckStateBad()
+            {
+#if DEBUG
+                //checking something
+#endif
+            }
+
+            [Conditional("DEBUG")]
+            void CheckState()
+            {
+                //checking something
+            }
+        }
+    }
+}
