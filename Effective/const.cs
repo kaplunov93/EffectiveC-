5cb440330ee9fa2868151f6dfb9342afa53b5e97
Aleksander Kaplunov <kaplunov93@gmail.com>
2015-08-10 13:39:30 +0300

    Chapter 1 Item 2

---
+++ Effective/const.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Effective
+{
+    namespace Const
+    {
+        
+        class Program
+        {
+            /// <summary>
+            /// Right, because more flexebility, this is run-time implementation
+            /// </summary>
+            public readonly string developer = "Alex Kaplunov";
+            /// <summary>
+            /// Wrong, this is compile time implemetation.
+            /// </summary>
+            public const string dev = "Alex Kaplunov";
+        }
+    }
+}
