15f9906566f4ecef48b19f7e6d8273a1bd272e40
Aleksander Kaplunov <kaplunov93@gmail.com>
2015-08-10 13:02:58 +0300

    Chapter 1 Item 1

---
+++ Effective/Properties.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Effective
+{
+    namespace Properties
+    {
+        /// <summary>
+        /// Right implement class with Property
+        /// </summary>
+        class Class
+        {
+            private string name;
+
+            public string Name
+            {
+                get
+                {
+                    return name;
+                }
+                set
+                {
+                    if (value != "")
+                        name = value;
+                }
+            }
+        }
+        /// <summary>
+        /// Wrong implement class with public variety
+        /// </summary>
+        class BadClass
+        {
+            public string Name;
+        }
+    }
+}
