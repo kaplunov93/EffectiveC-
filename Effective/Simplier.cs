606eb858cc5a95ffdf8d18f360f4b022d49c1a87
Aleksander Kaplunov <kaplunov93@gmail.com>
2015-08-11 18:36:35 +0300

    Chapter 1 Item 11

---
+++ Effective/Simplier.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Effective
+{
+    namespace Simplier
+    {
+        class Wrong
+        {
+            public void Work(bool What)
+            {
+                if (What)
+                    // Do something one
+                    //....
+                    return;
+                else
+                    // Do something else
+                    //.....
+                    return;
+
+            }
+        }
+
+        class Right
+        {
+            public void Work(bool What)
+            {
+                if (What)
+                    DoSomeOne();
+                else
+                    DoSomeElse();
+
+            }
+
+            private void DoSomeOne()
+            {
+                //Do something
+            }
+
+            private void DoSomeElse()
+            {
+                //Do something
+            }
+        }
+    }
+}
