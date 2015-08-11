8e3211ec16725beb15a2f7fd6df39c027ea0bb94
Aleksander Kaplunov <kaplunov93@gmail.com>
2015-08-11 17:07:48 +0300

    Chapter 1 Item 10

---
+++ Effective/Parameters.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Effective
+{
+    namespace Parameters
+    {
+        class Data
+        {
+            public string Name, Surname;
+            public int age;
+
+        }
+
+        class Wrong:Data
+        {
+            public Wrong()
+            {
+                Name = "Unknown";
+                Surname = "Unknown";
+                age = 0;
+            }
+
+            public Wrong(string name)
+            {
+                Name = name;
+                Surname = "Unknown";
+                age = 0;
+            }
+
+            // ........... 
+
+        }
+
+        class Right:Data
+        {
+            public Right(string name= "Unknown",string surname= "Unknown",int Age=0)
+            {
+                Name = name;
+                Surname = surname;
+                age = Age;
+            }
+        }
+    }
+}
