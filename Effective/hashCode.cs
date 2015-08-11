f8f82301be527c2bf8940e86fef7ab598ca55052
Aleksander Kaplunov <kaplunov93@gmail.com>
2015-08-11 12:29:24 +0300

    Chapter 1 Item 7

---
+++ Effective/hashCode.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Effective
+{
+    namespace hashCode
+    {
+        class RightClass
+        {
+            private string field;
+
+            public RightClass(string field)
+            {
+                this.field = field;
+            }
+
+            public string Field
+            {
+                get { return field; }
+            }
+
+            public override int GetHashCode()
+            {
+                return field.GetHashCode();
+            }
+
+            public RightClass ChangeField(string field)
+            {
+                return new RightClass(field);
+            }
+        }
+
+        class WrongClass
+        {
+            private string field;
+
+            public string Field
+            {
+                get { return field; }
+                set { field = value; }
+            }
+
+            public override int GetHashCode()
+            {
+                return field.GetHashCode();
+            }
+        }
+    }
+}
