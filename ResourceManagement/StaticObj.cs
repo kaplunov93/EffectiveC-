a5fd72f9cdde3e2ad6ec0a3bff1faef2e4de7c9d
Aleksander Kaplunov <kaplunov93@gmail.com>
2015-08-12 12:43:06 +0300

    Chapter 2 Item 13

---
+++ ResourceManagement/StaticObj.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResourceManagement
+{
+    namespace StaticObj
+    {
+        class Wrong
+        {
+            private static readonly Wrong obj = new Wrong();
+
+            public Wrong Obj { get { return obj; } }
+
+            private Wrong()
+            { }
+        }
+
+        class Right
+        {
+            private static readonly Right obj;
+
+            static Right()
+            { obj = new Right(); }
+
+            public Right Obj { get { return obj; } }
+
+            private Right()
+            { }
+        }
+    }
+}
