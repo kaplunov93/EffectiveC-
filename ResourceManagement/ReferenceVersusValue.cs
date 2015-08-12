13d2d417fc6ba1ac0675afcaa053fd80b59b1bd8
Aleksander Kaplunov <kaplunov93@gmail.com>
2015-08-12 18:42:19 +0300

    Chapter 2 Item 18

---
+++ ResourceManagement/ReferenceVersusValue.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResourceManagement
+{
+    namespace ReferenceVersusValue
+    {
+        // Values types
+        struct MyStruct
+        {
+            public string Name
+            {
+                get;
+                set;
+            }
+
+            public string SurnameName
+            {
+                get;
+                set;
+            }
+
+            public void Change(string name="",string surname="")
+            {
+                if (name != "")
+                    Name = name;
+                if (surname != "")
+                    SurnameName = surname;
+            }
+        }
+
+        //References types 
+        //better to use this variant
+        class MyClass
+        {
+            public string Name
+            {
+                get;
+                set;
+            }
+
+            public string SurnameName
+            {
+                get;
+                set;
+            }
+
+            public void Change(string name = "", string surname = "")
+            {
+                if (name != "")
+                    Name = name;
+                if (surname != "")
+                    SurnameName = surname;
+            }
+        }
+    }
+}
