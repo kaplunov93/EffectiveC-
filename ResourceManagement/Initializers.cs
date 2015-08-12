6f34581215e8cd806da75edd22741731916041a1
Aleksander Kaplunov <kaplunov93@gmail.com>
2015-08-12 12:20:56 +0300

    Chapter 2 Item 12

---
+++ ResourceManagement/Initializers.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResourceManagement
+{
+    namespace Initializers
+    {
+        class Wrong
+        {
+            private List<string> labels = new List<string>();
+
+            public Wrong()
+            {
+                labels=new List<string>();
+            }
+
+            public Wrong(int size)
+            {
+                labels = new List<string>(size);
+            }
+        }
+
+        class Right
+        {
+            private List<string> labels;
+
+            public Right()
+            {
+                labels = new List<string>();
+            }
+
+            public Right(int size)
+            {
+                labels = new List<string>(size);
+            }
+        }
+    }
+}
