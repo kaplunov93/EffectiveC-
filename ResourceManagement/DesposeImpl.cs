9b0f101cc3999e0699bdabeb063fe221a8939e21
Aleksander Kaplunov <kaplunov93@gmail.com>
2015-08-12 18:03:23 +0300

    Chapter 2 Item 17

---
+++ ResourceManagement/DesposeImpl.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResourceManagement
+{
+    namespace DesposeImpl
+    {
+        class Right : IDisposable
+        {
+            private bool IsDisposed = false;
+
+            public virtual void Dispose(bool disposing)
+            {
+                if (!IsDisposed)
+                {
+                    if (disposing)
+                    {
+                        //free managed resources
+                    }
+                    //free unmanaged resources
+                    IsDisposed = true;
+                }
+            }
+
+            public void Dispose()
+            {
+                Dispose(true);
+                GC.SuppressFinalize(this);
+            }
+
+            ~Right()
+            {
+                Dispose(false);
+            }
+        }
+
+        class RightDerived :Right
+        {
+            private bool isDisposed = false;
+
+            public override void Dispose(bool disposing)
+            {
+                if(!isDisposed)
+                {
+                    if(disposing)
+                    {
+                        //free managed resources
+                    }
+                    //free unmanaged resources
+                    isDisposed = true;
+                }
+                base.Dispose(disposing);
+            }
+        }
+    }
+}
