b3a0e7a87646844abc0fb4ecadf5f7b2710410da
Aleksander Kaplunov <kaplunov93@gmail.com>
2015-08-13 14:28:03 +0300

    Chapter 3 Item 21

---
+++ ExpressingDesigns/Visibility.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpressingDesigns
+{
+    namespace Visibility
+    {
+        public class PhoneNumber { }
+
+        //Wrong Idea
+        public class PhoneValidator
+        {
+            public bool PhoneValidate(PhoneNumber ph)
+            {
+                //Number Validate
+                return true;
+            }
+        }
+
+        //Right Idea
+        public interface IPhoneValidator
+        {
+            bool Validate(PhoneNumber ph);
+        }
+
+        internal class UsaPhoneValidator : IPhoneValidator
+        {
+            public bool Validate(PhoneNumber ph)
+            {
+                //validate Usa phone number
+                return true;
+            }
+        }
+
+        internal class UkrainePhoneValidator : IPhoneValidator
+        {
+            public bool Validate(PhoneNumber ph)
+            {
+                //validate Ukrainian phone number
+                return true;
+            }
+        }
+
+        internal class InternationalPhoneValidator : IPhoneValidator
+        {
+            public bool Validate(PhoneNumber ph)
+            {
+                //validate International phone number
+                return true;
+            }
+        }
+    }
+}
