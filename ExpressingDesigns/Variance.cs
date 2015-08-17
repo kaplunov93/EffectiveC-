65350b5f9fb8d3508a20353803ea6be0a280174d
Aleksander Kaplunov <kaplunov93@gmail.com>
2015-08-17 13:35:49 +0300

    Chapter 3 Item 29

---
+++ ExpressingDesigns/Variance.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpressingDesigns
+{
+    namespace Variance
+    {
+        //Covariant
+        interface My1<out T>
+        {
+            T Get();
+            Func<T> GetLater();
+            void GiveLater(Action<T> Doing);
+        }
+
+        //Contrvariant
+        interface My2<in T>
+        {
+            void Act(T item);
+            void GetLater(Func<T> item);
+            Action<T> ActLater();
+        }
+    }
+}
