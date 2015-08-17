67a98fb61564a96a9bedb01c080841c9451404df
Aleksander Kaplunov <kaplunov93@gmail.com>
2015-08-17 17:56:32 +0300

    Chapter 4 Item 34

---
+++ Framework/Overloads.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Framework
+{
+    namespace Overloads
+    {
+        class Base
+        {
+            void Method()
+            {
+                //body of method
+            }
+
+            void BaseMethod()
+            {
+                //body of method
+            }
+
+            virtual public void BaseMethod1() { }
+        }
+
+        class Derieved : Base
+        {
+            /// <summary>
+            /// Wrong 
+            /// </summary>
+            void Method()
+            {
+                //body of method
+            }
+            /// <summary>
+            /// Right
+            /// </summary>
+            void DerivedMethod()
+            {
+                //body of method
+            }
+            /// <summary>
+            /// Right
+            /// </summary>
+            public override void BaseMethod1()
+            {
+                base.BaseMethod1();
+            }
+        }
+    }
+}
