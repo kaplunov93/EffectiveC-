c786a35e035afe72d13eeec401cc0086b4675e17
Aleksander Kaplunov <kaplunov93@gmail.com>
2015-08-17 17:25:01 +0300

    Chapter 4 Item 33

---
+++ Framework/newModifier.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Framework
+{
+    namespace newModifier
+    {
+        class BaseClass
+        {
+            public void DoSomething()
+            {
+                //doing.........
+            }
+        }
+
+        class DerivedClass:BaseClass
+        {
+            public new void  DoSomething()
+            {
+                base.DoSomething();
+                //doing......
+            }
+        }
+    }
+}
