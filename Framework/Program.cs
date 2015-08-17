4aa50afbc22124d95a5abdb81e49cda378c8ae8e
Aleksander Kaplunov <kaplunov93@gmail.com>
2015-08-17 14:54:17 +0300

    Chapter 4 Item 30

---
+++ Framework/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Framework
+{
+    namespace Event
+    {
+        class Window
+        {
+            public virtual void OnAction(EventArgs e)
+            {
+                //Do Some Actions
+            }
+        }
+
+        class Window1 : Window
+        {
+            public Window1()
+            {
+                //initiliaze new Window
+            }
+
+            public override void OnAction(EventArgs e)
+            {
+                //Do Some Actions
+                base.OnAction(e);
+            }
+        }
+    }
+}
