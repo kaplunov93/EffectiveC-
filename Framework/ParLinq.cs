a181304233ad6d6c1c792d21b948b434efc4de72
Aleksander Kaplunov <kaplunov93@gmail.com>
2015-08-18 14:03:11 +0300

    Chapter 4 Item 35

---
+++ Framework/ParLinq.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Framework
+{
+    namespace ParLinq
+    {
+        /// <summary>
+        /// Parallel execution
+        /// </summary>
+        class LinqP
+        {
+            void main()
+            {
+                var numbers = from n in ParallelEnumerable.Range(0, 100)
+                              where n % 5 == 0
+                              select n;
+            }
+
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        class Linq
+        {
+            void main()
+            {
+                var numbers = from n in Enumerable.Range(0, 100)
+                              where n % 5 == 0
+                              select n;
+            }
+
+        }
+    }
+}
