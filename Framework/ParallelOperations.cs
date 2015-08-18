ed2ee788a917f066ceb806822c8053ac4e0f7af4
Aleksander Kaplunov <kaplunov93@gmail.com>
2015-08-18 15:34:08 +0300

    Chapter 4 Item 36

---
+++ Framework/ParallelOperations.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Framework
+{
+    namespace ParallelOperations
+    {
+        static class ParallelAsync
+        {
+            //run asynchoronous operation
+            public static void RunAsync<T, TResult>
+                (this IEnumerable<T> taskParms, 
+                Func<T, Task<TResult>> taskStarter, 
+                Action<Task<TResult>> taskFinisher)
+            {
+                taskParms.Select(parm => taskStarter(parm)).AsParallel().
+                    ForAll(t => t.ContinueWith(t2 => taskFinisher(t2)));
+            }
+        }
+
+        class Parallele
+        {
+            public void Loop<T>(List<T> table)
+            {
+                Parallel.ForEach<T>(table, item =>
+                 {
+                     Console.WriteLine(item.ToString());
+                 });
+            }
+
+            public void pLinq<T>(List<T> table)
+            {
+                table.ForEach(item=>Console.WriteLine(item));
+            }
+        }
+    }
+}
