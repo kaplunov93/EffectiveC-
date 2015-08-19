a9e09bd9f38c3b8a5ee0cf6a8ef16cfa01f51553
Aleksander Kaplunov <kaplunov93@gmail.com>
2015-08-19 15:41:41 +0300

    Chapter 5 Item 39

---
+++ DynamicProgramming/Conversation.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicProgramming
+{
+    namespace Conversation
+    {
+        static class Program
+        {
+
+
+            static void Main(string[] args)
+            {
+                List<string> strings = new List<string> { "Hello", "World", "Friends" };
+                IEnumerable<string> str = Convert<string>(strings);
+                foreach (string s in str)
+                {
+                    Console.WriteLine(s);
+                }
+
+                Console.ReadKey();
+
+            }
+
+            static IEnumerable<TResult> Convert<TResult>(this System.Collections.IEnumerable sequence)
+            {
+                foreach (var item in sequence)
+                {
+                    dynamic coercion = (dynamic)item;
+                    yield return (TResult)coercion;
+                }
+            }
+
+        }
+    }
+}
