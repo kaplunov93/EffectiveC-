73aad0ac258bd43d76610e166228bb2494d2dd33
Aleksander Kaplunov <kaplunov93@gmail.com>
2015-08-21 12:19:34 +0300

    Chapter 5 Item 44

---
+++ DynamicProgramming/MinDynToUser.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicProgramming
+{
+    namespace MinDynToUser
+    {
+        class Program
+        {
+
+
+            static void Main(string[] args)
+            {
+                int a = Add<int, double, int>(5, 5.5);
+                double b = Add<int, double, double>(5, 5.5);
+                Console.WriteLine("{0}\n{1}", a, b);
+
+                Console.ReadKey();
+            }
+
+            static dynamic DynamicAdd(dynamic left, dynamic right)
+            {
+                return left + right;
+            }
+
+            static TResult Add<T1, T2, TResult>(T1 left, T2 right)
+            {
+                return (TResult)DynamicAdd(left, right);
+            }
+        }
+    }
+}
