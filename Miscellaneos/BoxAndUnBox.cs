2eecbff50017e14be8b8721c5692bdf8b52f8a61
Aleksander Kaplunov <kaplunov93@gmail.com>
2015-08-21 13:06:30 +0300

    Chapter 6 Item 45

---
+++ Miscellaneos/BoxAndUnBox.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Miscellaneos
+{
+    namespace BoxAndUnBox
+    {
+        class Program
+        {
+
+
+            static void Main(string[] args)
+            {
+                //Boxing ints to objects
+                Console.WriteLine("{0}\t{1}\t{2}", 10, 20, 30);
+                //This operation without Boxing
+                Console.WriteLine("{0}\t{1}\t{2}", 10.ToString(), 20.ToString(), 30.ToString());
+
+                Console.ReadKey();
+            }
+
+
+        }
+    }
+}
